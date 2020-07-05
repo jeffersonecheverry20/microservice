9d672913c4d5170f1b871805a01b86f03874f5dc
Jefferson Echeverry <echeverryjefferson@gmail.com>
2020-07-04 20:40:40 -0500

    add admin-service with dependency

---
+++ admin-service/src/test/java/academy/jefferson/admin/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.jefferson.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
